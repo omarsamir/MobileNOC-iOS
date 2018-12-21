f6a1bb0fec7d49cf15b31a1f0352bd27df131fe0
Omar Samir <omarsamir@live.co.uk>
2018-12-21 17:31:09 +0200

    feat: creating project

---
+++ MobileNOCChallenge/MobileNOCChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileNOCChallenge
+//
+//  Created by Omar Samir on 12/21/18.
+//  Copyright © 2018 Omar Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
